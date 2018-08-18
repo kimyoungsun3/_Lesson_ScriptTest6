8fa574adbcf044575ff995d0c3af2588359c46f8
kimyoungsun <kyssmart@naver.com>
2018-08-18 17:02:58 +0900

    oo
    qq

---
+++ Assets/200/204 MethordOverloading/OverloadTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverloadTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Add (1, 1);
+		Add (1f, 1f);
+		Add (1, 1f);
+		Add ("My", "Name");		
+	}
+
+	int Add(int x, int y){
+		Debug.Log ("int add");
+		return x + y;
+	}
+
+	float Add(float x, float y){
+		Debug.Log ("float add");
+		return x + y;
+	}
+
+	string Add(string x, string y){
+		Debug.Log ("string add");
+		return x + y;
+	}
+}
