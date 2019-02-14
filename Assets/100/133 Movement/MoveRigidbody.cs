1f0d78bbad54d3eb924ef713e9dca612bb528818
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-02-14 16:35:26 +0900

    3333
    33333

---
+++ Assets/100/133 Movement/MoveRigidbody.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovementTest
+{
+	public class MoveRigidbody : MonoBehaviour
+	{
+		Transform trans;
+		Rigidbody rb;
+		public float speedMove = 2f;
+		public float speedTurn = 180f;
+
+		void Start()
+		{
+			trans = transform;
+			rb = GetComponent<Rigidbody>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float _h = Input.GetAxis("Horizontal");
+			float _v = Input.GetAxis("Vertical");
+
+			rb.MovePosition(rb.position + _v * trans.forward * speedMove * Time.deltaTime);
+			rb.MoveRotation(rb.rotation * Quaternion.Euler(_h * Vector3.up * speedTurn * Time.deltaTime));
+		}
+	}
+}
---
+++ Assets/100/133 Movement/MoveTransform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovementTest
+{
+	public class MoveTransform : MonoBehaviour
+	{
+		Transform trans;
+		public float speedMove = 2f;
+		public float speedTurn = 180f;
+
+		void Start()
+		{
+			trans = transform;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float _h = Input.GetAxis("Horizontal");
+			float _v = Input.GetAxis("Vertical");
+
+			trans.Translate(_v * Vector3.forward * speedMove * Time.deltaTime);
+			trans.Rotate(_h * Vector3.up * speedTurn * Time.deltaTime);
+		}
+	}
+}
