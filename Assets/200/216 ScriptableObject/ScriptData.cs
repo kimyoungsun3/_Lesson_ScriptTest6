09512d9d2d18654a941e1d14e2486996e8cd5c54
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-02-26 10:14:49 +0900

    555
    555

---
+++ Assets/200/216 ScriptableObject/ScriptData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjectTest
+{
+	[CreateAssetMenu(fileName ="SaveData", menuName ="ScriptDataTest/Data")]
+	public class ScriptData : ScriptableObject
+	{
+		public int idx = 0;
+		public void Double()
+		{
+			idx *= 2;
+		}
+	}
+}
---
+++ Assets/200/216 ScriptableObject/Ui_ScriptData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScriptableObjectTest
+{
+	public class Ui_ScriptData : MonoBehaviour
+	{
+		public ScriptData saveData;
+		public UILabel label;
+
+		public static int idxStatic = 0;
+		public int idxNormal = 0;
+		public UILabel label2;
+
+		private void Start()
+		{
+			if (saveData != null)
+			{
+				idxStatic++;
+				idxNormal++;
+				label2.text = idxStatic + ":" + idxNormal;
+
+				label.text = saveData.idx.ToString();
+			}
+		}
+
+		public void InvokeIncreaseData()
+		{
+			if(saveData != null)
+			{
+				saveData.idx++;
+				label.text = saveData.idx.ToString();
+			}
+		}
+
+		public void InvokeSubMethod()
+		{
+			if (saveData != null)
+			{
+				saveData.Double();
+				label.text = saveData.idx.ToString();
+			}
+		}
+
+		public int nextIdx;
+		public void InvokeNextScene()
+		{
+			//Scene _scene = SceneManager.GetActiveScene();
+			//int _idx = _scene.buildIndex;
+			//_idx += 1;
+			//Debug.Log("a " + _idx + ":" + _scene.buildIndex + ":" + SceneManager.sceneCount);
+			//_idx = _idx >= SceneManager.sceneCount ? 0 : SceneManager.sceneCount;
+			//Debug.Log("b " + _idx + ":" + _scene.buildIndex);
+			SceneManager.LoadScene(nextIdx);
+		}
+	}
+}
