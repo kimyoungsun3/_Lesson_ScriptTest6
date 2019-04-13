aae6def6412823db54e372d7f3b9b2cb85315c71
kimyoungsun3 <40338139+kimyoungsun3@users.noreply.github.com>
2019-04-13 11:27:35 +0900

    333
    3333

---
+++ Assets/400/400_Bounds/BoundsTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoundsTestNameSpace {
+	public class BoundsTest : MonoBehaviour {
+		BoxCollider2D collder;
+		Bounds bounds;
+		public bool bUpdate, bExpand;
+		void Start() {
+			collder = GetComponent<BoxCollider2D>();
+			bounds = collder.bounds;
+		}
+
+		// Update is called once per frame
+		void Update() {
+			if (bUpdate)
+			{
+				bounds = collder.bounds;
+			}
+			else if (bExpand)
+			{
+				bExpand = false;
+				float _amount = 0.015f * -2;
+				Debug.Log(_amount);
+				bounds.Expand(_amount);
+
+			}
+			Debug.Log(
+				bounds.center
+				+ ":" + bounds.min
+				+ ":" + bounds.max
+				+ ":" + bounds.extents
+				+ ":" + bounds.size
+				+ ":" + bounds.size.x
+				);
+		}
+	}
+}
