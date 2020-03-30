d2ef43dc5f86ac4b993536b892e4cee3125d2e4a
kimyoungsun3 <kyssmart@naver.com>
2020-03-31 03:43:24 +0900

    222
    22222

---
+++ Assets/100/157_WaterWave/Spawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterWave
+{
+	public class Spawer : MonoBehaviour
+	{
+		[SerializeField] GameObject target;
+		[SerializeField] float distance = 0.2f;
+		// Use this for initialization
+		void Start()
+		{
+			Vector3 _pos = target.transform.position;
+			for (int i = 0; i < 100; i++)
+			{
+				_pos.x += distance;
+				Instantiate(target, _pos, Quaternion.identity);
+			}
+		}
+
+
+	}
+}
---
+++ Assets/100/157_WaterWave/WavePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterWave
+{
+	public class WavePoint : MonoBehaviour
+	{
+		Vector3 startPos;
+		Transform trans;
+		// Use this for initialization
+		void Start()
+		{
+			trans		= transform;
+			startPos	= trans.position;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			Vector3 _pos	= trans.position;
+			float _theta	= startPos.x + Time.time;
+
+			float _dx = Mathf.Cos(_theta);
+			float _dy = Mathf.Sin(_theta);
+			_pos.x = startPos.x + _dx;
+			_pos.y = startPos.y + _dy;
+			trans.position = _pos;
+		}
+	}
+}
