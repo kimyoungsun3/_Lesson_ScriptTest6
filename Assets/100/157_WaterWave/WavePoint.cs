4f93bc2e9aee1fe6cf131bdd4a58f62c5ecd080b
kimyoungsun3 <kyssmart@naver.com>
2020-03-31 12:33:48 +0900

    22
    222

--- Assets/100/157_WaterWave/WavePoint.cs
+++ Assets/100/157_WaterWave/WavePoint.cs
@@ -6,7 +6,7 @@ namespace WaterWave
 {
 	public class WavePoint : MonoBehaviour
 	{
-		Vector3 startPos;
+		Vector3 startPos, pos;
 		Transform trans;
 		// Use this for initialization
 		void Start()
@@ -18,14 +18,11 @@ namespace WaterWave
 		// Update is called once per frame
 		void Update()
 		{
-			Vector3 _pos	= trans.position;
 			float _theta	= startPos.x + Time.time;
-
 			float _dx = Mathf.Cos(_theta);
 			float _dy = Mathf.Sin(_theta);
-			_pos.x = startPos.x + _dx;
-			_pos.y = startPos.y + _dy;
-			trans.position = _pos;
+			pos.Set(startPos.x + _dx, startPos.y + _dy, startPos.z);
+			trans.position = pos;
 		}
 	}
 }
