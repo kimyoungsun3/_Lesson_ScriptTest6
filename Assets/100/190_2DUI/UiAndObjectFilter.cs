46caaccaf6a0f80532db40b7fffbbd0c68acf5d5
kimyoungsun3 <kyssmart@naver.com>
2020-04-22 13:57:45 +0900

    3333

---
+++ Assets/100/190_2DUI/UiAndObjectFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _190_UiTest
+{
+	public class UiAndObjectFilter : MonoBehaviour
+	{
+		Renderer renderer;
+		Color colorOri;
+
+		private void Start()
+		{
+			renderer = GetComponent<Renderer>();
+			colorOri = renderer.material.color;
+		}
+
+		private void OnMouseEnter()
+		{
+			if (EventSystem.current.IsPointerOverGameObject())
+				return;
+			renderer.material.color = Color.green;
+		}
+
+		private void OnMouseExit()
+		{
+			renderer.material.color = colorOri;
+		}
+	}
+}
