84699f824259f9832d2026a67a99763e32b97869
kimyoungsun3 <kyssmart@naver.com>
2020-04-29 13:54:21 +0900

    4444

---
+++ Assets/100/113_TransformSpace/ForwardMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _113_TransformSpace
+{
+	public class ForwardMove : MonoBehaviour
+	{
+		[SerializeField] float speed = 2f;
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float _v = Input.GetAxisRaw("Vertical");
+			if (_v != 0)
+				transform.Translate(_v * Vector3.forward * speed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/100/124_Prefab/PrefabTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _124_PrefabTest
+{
+	public class PrefabTest : MonoBehaviour
+	{
+		public float floatValue = 100f;
+		public Vector3 vectorValue = new Vector3(1, 2, 3);
+		public GameObject go1, go2, go3;
+		public GameObject goCamera;
+		public GameObject prefabCube;
+	}
+}
