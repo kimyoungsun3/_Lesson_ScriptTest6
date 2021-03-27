e0ac5041af6faac5f37c80534fe3a3b069346d71
kimyoungsun3 <kyssmart@naver.com>
2021-03-27 15:04:25 +0900

    2222

---
+++ Assets/200/223/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Step223
+{
+	public class PlayerMove : MonoBehaviour
+	{
+		public float speed = 2f;
+		Transform trans;
+		void Start()
+		{
+			trans = transform;
+		}
+
+		void Update()
+		{
+			float _v = Input.GetAxisRaw("Vertical");
+			float _h = Input.GetAxisRaw("Horizontal");
+			Vector3 _move = new Vector3(_h, 0, _v);
+
+			trans.Translate(_move.normalized * speed * Time.deltaTime);
+
+		}
+	}
+}
